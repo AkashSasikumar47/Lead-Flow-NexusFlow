ed103d718ef1ff5774de5c3b24c7d62d797b1879
SaiSatya16 <jj1039@srmist.edu.in>
2023-09-15 10:57:38 +0530

    update

--- app.py
+++ app.py
@@ -10,5 +10,25 @@ app.app_context().push()
 def home():
     return render_template('index.html')
 
+# /ST-general-settings
+@app.route('/ST-general-settings',methods=['GET','POST'])
+def settings():
+    return render_template('ST-general-settings.html')
+
+#/pages-login
+
+@app.route('/pages-login',methods=['GET','POST'])
+def login():
+    return render_template('pages-login.html')
+
+#/M-Inquiries
+
+@app.route('/M-Inquiries',methods=['GET','POST'])
+def Inquiries():
+    return render_template('M-Inquiries.html')
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
